63ebdc7b15edaf61eb028dd3b1d46f6fe4bc833a
selti <info@hexgecko.com>
2016-09-01 01:32:57 +0200

    Updated the single-page website with a fsm debug information.

--- docs/single-page-website/app/app.js
+++ docs/single-page-website/app/app.js
@@ -31,6 +31,47 @@ var pageList = {
 // create a new fsm object with the first state of '!'
 var websiteState = new FSM('!');
 
+// rewrite the debug table by hookinh the table update 
+websiteState.hookTableUpdate(function(tbl) {  
+  // format the table header
+  var debugTableStr = "<table class='fsm-table'>";
+  debugTableStr += "<tr><th>Current State</th><th>Event</th><th>Next State</th><th>Action</th></tr>";
+  
+  // format the table data
+  for(var currentState in tbl) {
+    for(var event in tbl[currentState]) {
+      var n = tbl[currentState][event];
+      debugTableStr +=
+        "<tr>" + 
+          "<td>" + currentState + "</td>" +
+          "<td>" + event + "</td>" + 
+          "<td>" + n.next + "</td>" +
+          "<td>" + (typeof n.action === "function" ? n.action.name : "") + "</td>" +
+        "</tr>";
+    }
+  }
+  debugTableStr += "</table>";
+  
+  // append it to the debug info element
+  $('#debug-info').empty().html(debugTableStr);
+});
+
+// update the current selected state and the current the last event
+websiteState.hookStateUpdate(function(currentState, event, nextState, action) {  
+  var $rows = $(".fsm-table tr").removeClass('last-event').removeClass('current-state');
+  
+  for(var i=0; i<$rows.length; i++) {
+    var contentText = $rows[i].firstChild.textContent;
+    if(contentText === currentState && $rows[i].children[1].textContent === event) {
+      $rows[i].setAttribute('class', 'last-event');
+    }
+    
+    if(contentText === nextState) {
+      $rows[i].setAttribute('class', 'current-state');
+    }
+  }
+});
+
 // fire a ready event when the document is loaded
 $(document).ready(function() {
   websiteState.fireEvent('ready');
@@ -38,7 +79,6 @@ $(document).ready(function() {
 
 // append the initalize state '!'
 websiteState.append(function() {
-  
   // called when the document is ready, initalize the webpage
   function initalize() {
     // if no hash address is given, go to the home page
@@ -61,11 +101,21 @@ websiteState.append(function() {
       }
     });
     
+    // toggle the debug information if the user click on the footer
+    $('#toggle-debug').on('click', function() {
+      $('#debug-info').toggleClass('hide');
+    });
+    
     // finnaly load the current hash location
     var hash = location.hash;
     var page = pageList[hash];
     websiteState.fireEvent('load' + hash, page.menuItem, page.templateUrl);
   }
+  
+  // this can happen if the user tries to change the address while the page is loading
+  websiteState.hookErrorMissingState(function() {
+    location.reload();
+  });
   
   return [
     // setup the app when the document is ready
@@ -118,9 +168,6 @@ for(var hash in pageList) {
         
         // go to the @home state when the page is loaded
         ['loading' + hash, 'ready', hash, pageReady],
-        
-        // try to reload the page if the user change the hash url while the page is loading
-        [/^loading#\w+/, 'load' + hash, '!', reloadPage]
       ];
     });
   })(hash);
